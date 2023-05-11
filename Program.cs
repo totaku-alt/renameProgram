f2e63f64df62058c72f6d434045b97f54e15f97c
Tobias <tobiasheinen@live.com>
2023-05-11 12:12:12 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+}
+
+class Matcher {
+    public static List<string> matcher(string oldName, string newName, List<string> files) {
+        return files;
+    }
+}
